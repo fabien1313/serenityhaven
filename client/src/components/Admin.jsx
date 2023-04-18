65f32f6b39c187747b5db06a5cbb8e365bafbdde
fabien moreno <fabienmoreno1331@yahoo.com>
2023-04-18 17:17:22 -0500

    structure and components setup

---
+++ client/src/components/Admin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div className='admin-main-container'>Admin</div>
+  )
+}
+
+export default Admin
---
+++ client/src/components/App.jsx
@@ -0,0 +1,18 @@
+import Home from "./Home"
+import Dashboard from "./Dashboard"
+import Admin from "./Admin"
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Home />
+      <Dashboard />
+      <Admin />
+    </div>
+  )
+}
+
+export default App
