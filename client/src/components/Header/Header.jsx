168bfa55b3af8fae999cc8c5945c50a743d1b347
fabien moreno <fabienmoreno1331@yahoo.com>
2023-04-19 20:52:30 -0500

    header working. Need to develop burger menu

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './header.css'
 import logo from '../../images/icons/apartments3_256.png'
-import logo2 from '../../images/icons/greenBlueTriangle_256.png'
+
 
 const Header = () => {
+
+  
+
+
+
   return (
     <header id="header-main-container">
 
-      <div id="header-logo-title">
-        Serenity Haven
+  <div id="header-container-all-elems-div">
+      <div id="header-container-logo-img">
+        <div id="header-logo-title">
+          Serenity Haven
+        </div>
+        <div id="header-img-div">
+          <img id="header-logo-img" src={logo} alt="logo" />
+        </div>
       </div>
-      <div>
-        <img id="header-logo-img" src={logo} alt="logo" />
 
+      <div id="header-nav-div">
+        <h3 className='header-nav-item1'>nav1</h3>
+        <h3 className='header-nav-item2'>nav2</h3>
+        <h3 className='header-nav-item3'>nav3</h3>
+        <h3 className='header-nav-item4'>nav4</h3>
       </div>
+  </div>
 
 
 
